5ce2d824a4fde3b06e141d3bd51389107497ff55
brotherilia <ilyamenshikov@gmail.com>
2016-04-03 03:18:44 +0300

    Автоматизация сборки. Проверка по критериям.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,49 @@
 module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
+  require('time-grunt')(grunt);
+
   grunt.initConfig({
+
+    clean: {
+      build: ["build"]
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/*.{jpg,png,svg}",
+            "tmp/ugc/*",
+            "js/**/*.js",
+            "video/**",
+            "*.html"
+          ],
+          dest: "build"
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          src: ["*.html"],
+          dest: "build"
+        }]
+      },
+      js: {
+        files: [{
+          expand: true,
+          src: ["js/**/*.js"],
+          dest: "build"
+        }]
+      }
+    },
+
     less: {
       style: {
         files: {
-          "css/style.css": "less/style.less"
+          "build/css/style.css": "less/style.less"
         }
       }
     },
@@ -21,37 +59,34 @@ module.exports = function(grunt) {
             "last 2 Firefox versions",
             "last 2 Opera versions",
             "last 2 Edge versions"
-          ]})
+          ]}),
+          require("css-mqpacker")({
+            sort: true
+          })
         ]
       },
       style: {
-        src: "css/*.css"
+        src: "build/css/*.css"
       }
     },
 
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: [
-            "*.html",
-            "css/*.css"
-          ]
-        },
+    csso: {
+      style: {
         options: {
-          server: ".",
-          watchTask: true,
-          notify: false,
-          open: true,
-          ui: false
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
         }
       }
     },
 
-    watch: {
-      files: ["less/**/*.less"],
-      tasks: ["less", "postcss"],
-      options: {
-        spawn: false
+    svgmin: {
+      allsvg: {
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.svg"]
+        }]
       }
     },
 
@@ -63,29 +98,95 @@ module.exports = function(grunt) {
       },
       sprite: {
         files: {
-          "img/sprite.svg": ["img/sprite/*.svg"]
+          "build/img/sprite.svg": ["img/sprite/*.svg"]
         }
       }
     },
 
-    svgmin: {
-      all: {
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
         files: [{
           expand: true,
-          src: ["img/**/*.svg"]
+          src: ["build/img/**/*.{png,jpg,gif}","build/tmp/ugc/**/*.{png,jpg,gif}"]
         }]
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
       },
-      sprite: {
-        files: [{
-          expand: true,
-          src: ["img/sprite/*.svg"]
-        }]
+      scripts: {
+        files: {
+          'build/js/picturefill.min.js': ['build/js/picturefill.js'],
+          'build/js/script.min.js': ['build/js/script.js']
+        }
+      }
+    },
+
+    processhtml: {
+      target: {
+        files: {
+          'build/index.html': ['build/index.html'],
+          'build/photo.html': ['build/photo.html'],
+          'build/form.html': ['build/form.html']
+        }
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css",
+            "build/js/*.js"
+          ]
+        },
+        options: {
+          server: "build/",
+          watchTask: true,
+          notify: false,
+          open: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ["*.html"],
+        tasks: ["copy:html", "processhtml"],
+        options: {spawn: false}
+      },
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss", "csso"],
+        options: {spawn: false}
+      },
+      scripts: {
+        files: ["js/**/*.js"],
+        tasks: ["copy:js","uglify"],
+        options: {spawn: false}
       }
     }
 
   });
 
-  grunt.registerTask("svgminall", "svgmin:all");
-  grunt.registerTask("sprite", ["svgmin:sprite", "svgstore"]);
   grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask("svg", ["svgmin", "svgstore"]);
+  grunt.registerTask("css", ["less", "postcss", "csso"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "css",
+    "svg",
+    "imagemin",
+    "uglify",
+    "processhtml"
+  ]);
+
 };
