ff6094909580e52daa80ebd14bb596b545bad024
brotherilia <ilyamenshikov@gmail.com>
2016-04-13 02:44:42 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var menuIcon  = document.querySelector('.main-nav__icon'),
+    menuItems = document.querySelector('.main-nav__items');
+    if (!(menuItems.classList.contains('main-nav__items--closed'))) {
+        menuItems.classList.add('main-nav__items--closed')
+        menuIcon.classList.add('main-nav__icon--closed')
+    }
+    menuIcon.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (menuItems.classList.contains('main-nav__items--closed')) {
+        menuItems.classList.remove('main-nav__items--closed')
+        menuIcon.classList.remove('main-nav__icon--closed')
+    }
+    else {
+        menuItems.classList.add('main-nav__items--closed');
+        menuIcon.classList.add('main-nav__icon--closed');
+    }
+  });
